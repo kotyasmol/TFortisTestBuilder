58d4240c738695973046c6895ed79774a14338ed
Rostislav Zyryanov <rost0406s2004@yandex.ru>
2026-05-05 15:26:02 +0500

    Добавлено сворачивание панели нод
    Кнопка-таб  меню нод сворачивает/разворачивает её.
    В свёрнутом состоянии остаётся только узкая кнопка.
    IsPaletteCollapsed и TogglePaletteCommand добавлены в TestViewModel.

--- TestBuilder/ViewModels/TestViewModel.cs
+++ TestBuilder/ViewModels/TestViewModel.cs
@@ -55,6 +55,14 @@ public partial class TestViewModel : ViewModelBase, IGraphEditor, IExecutionObse
     [ObservableProperty]
     private bool canGoBackGraph;
 
+    [ObservableProperty]
+    private bool isPaletteCollapsed;
+
+    public string PaletteToggleIcon => IsPaletteCollapsed ? "‹" : "›";
+    public string PaletteToggleTip => IsPaletteCollapsed ? "Развернуть палитру" : "Свернуть палитру";
+
+    public ICommand TogglePaletteCommand { get; }
+
     public GraphWorkspaceViewModel RootGraph { get; } = new()
     {
         Title = "Основной граф",
@@ -184,10 +192,17 @@ public partial class TestViewModel : ViewModelBase, IGraphEditor, IExecutionObse
         SaveGraphCommand = new AsyncRelayCommand(SaveGraphAsync);
         LoadProfileCommand = new AsyncRelayCommand(async () => RefreshProfiles());
         ImportProfilesCommand = new AsyncRelayCommand(ImportProfilesAsync);
+        TogglePaletteCommand = new RelayCommand(() => IsPaletteCollapsed = !IsPaletteCollapsed);
 
         RefreshProfiles();
 
         StatusMessage = "Готов к подключению.";
+    }
+
+    partial void OnIsPaletteCollapsedChanged(bool value)
+    {
+        OnPropertyChanged(nameof(PaletteToggleIcon));
+        OnPropertyChanged(nameof(PaletteToggleTip));
     }
 
     partial void OnCurrentGraphChanged(GraphWorkspaceViewModel value)
